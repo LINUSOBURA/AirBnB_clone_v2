7884a09d12ea4c2c7cbb2896c41710c0562723d6
LINUSOBURA <linusobura@gmail.com>
2024-04-05 02:39:41 +0300

    updated sed

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -15,6 +15,6 @@ sudo ln -sfn /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data/
 
-sudo sed -i '15 i \\n\tlocation /hbnb_static {\n\talias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
 service nginx restart
