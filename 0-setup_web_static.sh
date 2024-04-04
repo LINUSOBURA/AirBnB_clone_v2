b9b72e0c9376bc28b88a7523b828675434e35ab6
LINUSOBURA <linusobura@gmail.com>
2024-04-05 02:18:05 +0300

    reverted to normal

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,27 +1,21 @@
 #!/usr/bin/env bash
-# script that sets up web servers for the deployment of web_static
+# Preparing web servers for deployment of web_static
 sudo apt-get update
 sudo apt-get -y install nginx
-sudo ufw allow 'Nginx HTTP'
 
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/
+#creating directories and files
 sudo mkdir -p /data/web_static/releases/test/
-sudo touch /data/web_static/releases/test/index.html
-sudo echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
+sudo mkdir -p /data/web_static/shared/
+echo "<h1>Testing Deployment</h1>" > /data/web_static/releases/test/index.html
 
-sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
+#Creating symbolic link
+sudo ln -sfn /data/web_static/releases/test/ /data/web_static/current
 
+#Changing ownership
 sudo chown -R ubuntu:ubuntu /data/
 
-sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+#inserting ne location into nginx configuration server block
 
-sudo service nginx restart
+sudo sed -i '15 i \\n\tlocation /hbnb_static {\n\talias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
+
+service nginx restart
