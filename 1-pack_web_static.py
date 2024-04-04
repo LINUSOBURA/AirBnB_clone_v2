ba56db414a5190f7e726d605f99c2fba009c94f0
LINUSOBURA <linusobura@gmail.com>
2024-04-04 15:31:11 +0300

    Fixed pcs and added commments

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-"""Fabric script that generates a .tgz archive from the contents of the web_static folder of your AirBnB Clone repo, using the function do_pack"""
+"""
+Fabric script that generates a .tgz archive from
+the contents of the web_static folder of your
+AirBnB Clone repo, using the function do_pack
+"""
 
 import os
 from datetime import datetime
@@ -8,11 +12,8 @@ from fabric.api import *
 
 
 def do_pack():
-    versions_folder = "versions"
-    try:
-        os.makedirs(versions_folder)
-    except:
-        pass
+    """generates a .tgz archive from the contents of the web_static folder"""
+    local("sudo mkdir -p versions")
     now = datetime.now()
     date = now.strftime("%Y%m%d%H%M%S")
     local(f"tar -cvzf versions/web_static_{date}.tgz web_static")
--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""Fabric script that generates a .tgz archive from the contents of the web_static folder of your AirBnB Clone repo, using the function do_pack"""
+"""
+abric script (based on the file 1-pack_web_static.py)
+that distributes an archive to your web servers, using the function do_deploy
+"""
 
 import os
 from datetime import datetime
@@ -10,17 +13,16 @@ env.hosts = ['3.85.177.85', '18.206.192.69']
 
 
 def do_pack():
-    versions_folder = "versions"
-    try:
-        os.makedirs(versions_folder)
-    except:
-        pass
+    """generates a .tgz archive from the contents of the web_static folder"""
+    local("sudo mkdir -p versions")
     now = datetime.now()
     date = now.strftime("%Y%m%d%H%M%S")
     local(f"tar -cvzf versions/webstatic_{date}.tgz web_static")
 
 
 def do_deploy(archive_path):
+    """ Deploying the archive to web servers """
+    """ Returns False if the file at the path archive_path doesn’t exist """
     if not archive_path or not os.path.exists(archive_path):
         return False
 
@@ -28,25 +30,26 @@ def do_deploy(archive_path):
     arch_name_ntgz = arch_name.split('.')[0]
 
     try:
+        """ Upload the archive to the /tmp/ directory of the web server """
         put(archive_path, f"/tmp/{arch_name}")
 
         run(f"mkdir -p /data/web_static/releases/{arch_name_ntgz}/")
-
-        run(f"tar -xzvf /tmp/{arch_name} -C /data/web_static/releases/{arch_name_ntgz}/"
-            )
-
+        """ Uncompress the archive """
+        run(f"tar -xzvf /tmp/{arch_name} -C\
+            /data/web_static/releases/{arch_name_ntgz}/")
+        """ Delete the archive from the web server """
         run(f"rm /tmp/{arch_name}")
 
-        run(f"mv /data/web_static/releases/{arch_name_ntgz}/web_static/* /data/web_static/releases/{arch_name_ntgz}"
-            )
+        run(f"mv /data/web_static/releases/{arch_name_ntgz}/web_static/*\
+            /data/web_static/releases/{arch_name_ntgz}")
 
         run(f"rm -rf /data/web_static/releases/{arch_name_ntgz}/web_static")
-
+        """ Delete the symbolic link /data/web_static/current """
         run("rm -rf /data/web_static/current")
-
-        run(f"ln -s /data/web_static/releases/{arch_name_ntgz}/ /data/web_static/current"
-            )
-    except:
+        """ Create a new the symbolic link /data/web_static/current """
+        run(f"ln -s /data/web_static/releases/{arch_name_ntgz}/\
+            /data/web_static/current")
+    except Exception as e:
         return False
 
     return True
