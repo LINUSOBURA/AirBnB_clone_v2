a3d1257e623a724de7f9ffff4706a5acc4402b4a
LINUSOBURA <linusobura@gmail.com>
2024-04-05 02:10:53 +0300

    temp code_ remove after

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,53 +1,32 @@
 #!/usr/bin/python3
 """
-abric script (based on the file 1-pack_web_static.py)
-that distributes an archive to your web servers, using the function do_deploy
+Fabric script based on the file 1-pack_web_static.py that distributes an
+archive to the web servers
 """
 
-import os
-from datetime import datetime
+from os.path import exists
 
-from fabric.api import *
+from fabric.api import env, put, run
 
-env.hosts = ["3.85.177.85", "18.206.192.69"]
-
-
-def do_pack():
-    """generates a .tgz archive from the contents of the web_static folder"""
-    local("sudo mkdir -p versions")
-    now = datetime.now()
-    date = now.strftime("%Y%m%d%H%M%S")
-    local("tar -cvzf versions/web_static_{}.tgz web_static".format(date))
+env.hosts = ['54.89.109.87', '100.25.190.21']
 
 
 def do_deploy(archive_path):
-    """ Deploying the archive to web servers
-
-    Returns False if the file at the path archive_path doesn’t exist """
-    if not os.path.exists(archive_path):
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
         return False
-
-    arch_name = archive_path.split('/')[-1]
-    arch_name_ntgz = '/data/web_static/releases/{}'.format(
-        arch_name.split('.')[0])
-
     try:
-        """ Upload the archive to the /tmp/ directory of the web server """
-        put(archive_path, '/tmp/{}'.format(arch_name))
-
-        run('mkdir -p {}/'.format(arch_name_ntgz))
-        """ Uncompress the archive """
-        run("tar -xzvf /tmp/{} -C {}/".format(arch_name, arch_name_ntgz))
-        """ Delete the archive from the web server """
-        run('rm /tmp/{}'.format(arch_name))
-
-        run('mv {}/web_static/* {}'.format(arch_name_ntgz, arch_name_ntgz))
-
-        run('rm -rf {}/web_static'.format(arch_name_ntgz))
-        """ Delete the symbolic link /data/web_static/current """
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
         run('rm -rf /data/web_static/current')
-        """ Create a new the symbolic link /data/web_static/current """
-        run('ln -s {}/ /data/web_static/current'.format(arch_name_ntgz))
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
-    except Exception:
+    except:
         return False
