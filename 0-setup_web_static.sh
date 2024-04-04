37779caf4fcafbbca144b92277f95511c8810dd5
LINUSOBURA <linusobura@gmail.com>
2024-04-05 02:27:04 +0300

    rm comments

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,7 +3,6 @@
 sudo apt-get update
 sudo apt-get -y install nginx
 
-#creating directories and files
 sudo mkdir -p /data/
 sudo mkdir -p /data/web_static/
 sudo mkdir -p /data/web_static/releases/
@@ -12,13 +11,9 @@ sudo mkdir -p /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
 echo "<h1>Testing Deployment</h1>" > /data/web_static/releases/test/index.html
 
-#Creating symbolic link
 sudo ln -sfn /data/web_static/releases/test/ /data/web_static/current
 
-#Changing ownership
 sudo chown -R ubuntu:ubuntu /data/
-
-#inserting ne location into nginx configuration server block
 
 sudo sed -i '15 i \\n\tlocation /hbnb_static {\n\talias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
 
