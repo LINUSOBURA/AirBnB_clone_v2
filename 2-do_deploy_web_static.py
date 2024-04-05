93013e99833346adff18c22ebbbde4da26f890b6
LINUSOBURA <linusobura@gmail.com>
2024-04-05 20:51:54 +0300

    Temp 2 delete after

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
 """
-Fabric script based on the file 1-pack_web_static.py that distributes an
-archive to the web servers
+abric script (based on the file 1-pack_web_static.py)
+that distributes an archive to your web servers, using the function do_deploy
 """
 
+from datetime import datetime
 from os.path import exists
 
-from fabric.api import env, put, run
+from fabric.api import *
 
-env.hosts = ['54.89.109.87', '100.25.190.21']
+env.hosts = ["3.85.177.85", "18.206.192.69"]
+
+
+def do_pack():
+    """generates a .tgz archive from the contents of the web_static folder"""
+    local("sudo mkdir -p versions")
+    now = datetime.now()
+    date = now.strftime("%Y%m%d%H%M%S")
+    local("tar -cvzf versions/web_static_{}.tgz web_static".format(date))
 
 
 def do_deploy(archive_path):
