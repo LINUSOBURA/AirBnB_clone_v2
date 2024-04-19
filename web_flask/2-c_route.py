e88bf8670123586a710d54febb5cb579787565e8
LINUSOBURA <linusobura@gmail.com>
2024-04-19 15:25:40 +0300

    2&3

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+script that starts a Flask web application
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    mod_text = text.replace('_', ' ')
+    return "C " + mod_text
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
---
+++ web_flask/3-python_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+script that starts a Flask web application
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    mod_text = text.replace('_', ' ')
+    return "C " + mod_text
+
+
+@app.route("/python/<text>", strict_slashes=False)
+@app.route("/python/", defaults={'text': None}, strict_slashes=False)
+def py_text(text):
+    if text is not None:
+        mod_text = text.replace('_', ' ')
+        return "Python " + mod_text
+    else:
+        return "Python is cool"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
