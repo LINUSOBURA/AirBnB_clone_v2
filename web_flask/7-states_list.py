53902595e5cb4a48f02eb25b7f40ca449daf091d
LINUSOBURA <linusobura@gmail.com>
2024-04-22 13:45:39 +0300

    7-states fixed

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -11,17 +11,18 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def teardown_storage(exception):
-    storage.close()
-
-
 @app.route("/states_list", strict_slashes=False)
 def states_list():
     """Get states sorted by names"""
     states_data = sorted(list(storage.all(State).values()),
                          key=lambda x: x.name)
     return render_template('7-states_list.html', states=states_data)
+
+
+@app.teardown_appcontext
+def teardown_storage(exception):
+    """closes the storage on teardown"""
+    storage.close()
 
 
 if __name__ == "__main__":
