8fa7fd1408a07244be1d43dc338476d6c20fb695
LINUSOBURA <linusobura@gmail.com>
2024-04-22 13:57:49 +0300

    7-states fixed

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 
 @app.route("/states_list", strict_slashes=False)
 def states_list():
-    """ Get states sorted by names """
+    """ Get states sorted by names and render on template """
     states_data = sorted(list(storage.all(State).values()),
                          key=lambda x: x.name)
     return render_template('7-states_list.html', states=states_data)
