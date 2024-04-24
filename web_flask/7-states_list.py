26ce0ee50ac60135e6cdebcf1f2e10aaa275efde
LINUSOBURA <linusobura@gmail.com>
2024-04-24 15:35:24 +0300

    fix 7 documentation

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 @app.route("/states_list", strict_slashes=False)
 def states_list():
-    """ Get states sorted by names and render on template """
+    """Get states sorted by names and render on template"""
     states_data = sorted(list(storage.all(State).values()),
                          key=lambda x: x.name)
     return render_template('7-states_list.html', states=states_data)
@@ -19,7 +19,7 @@ def states_list():
 
 @app.teardown_appcontext
 def teardown_storage(exception):
-    """ closes the storage on teardown """
+    """closes the storage on teardown"""
     storage.close()
 
 
