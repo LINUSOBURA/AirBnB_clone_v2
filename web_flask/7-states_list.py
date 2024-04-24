19c8a0616b0ed710a7e80014d49318699558b1c1
LINUSOBURA <linusobura@gmail.com>
2024-04-24 10:29:08 +0300

    trying fixing 7-states doc| delete after

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,29 +1,27 @@
 #!/usr/bin/python3
 """
-script that starts a Flask web application
+starts a Flask web application
 """
 
 from flask import Flask, render_template
+from models import *
 from models import storage
-from models.state import State
 
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
+@app.route('/states_list', strict_slashes=False)
 def states_list():
-    """ Get states sorted by names and render on template """
-    states_data = sorted(list(storage.all(State).values()),
-                         key=lambda x: x.name)
-    return render_template('7-states_list.html', states=states_data)
+    """display a HTML page with the states listed in alphabetical order"""
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def teardown_storage(exception):
-    """ closes the storage on teardown """
+def teardown_db(exception):
+    """closes the storage on teardown"""
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
